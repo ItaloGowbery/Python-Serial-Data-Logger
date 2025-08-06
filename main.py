6023f14991d465d3a4a523a46a394585d24a5535
ItaloGowbery <italogowbery@hotmail.com>
2025-08-06 11:28:52 -0300

    feat: port menu
    adicionado menu para escolha de porta da USB

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
-#import tkinter as tk
+import serial
+import serial.tools.list_ports
+import threading
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+from tkinter import filedialog
+
+ser = None
+baud_rate = 9600
+running = False
+data = []
+
+def list_serial_ports():
+    ports = serial.tools.list_ports.comports()
+    return [port.device for port in ports]
+
+
 
 root = ttk.Window(themename="superhero")
-
 root.geometry("800x500")
 root.title("ENose")
 
 
+
 label = ttk.Label(root, text="ENose", font=("Arial", 18))
-label.pack(padx=0, pady=20)
+label.pack(padx=0, pady=0)
 
+#READ DELAY
 delaybox = ttk.Spinbox(bootstyle=SECONDARY)
-delaybox.pack(side=LEFT, anchor=N, padx=10, pady=10)
+delaybox.pack(side=LEFT, anchor=N, padx=10, pady=0)
 
-menuCOM = ttk.Menubutton(bootstyle=SECONDARY)
-menuCOM.pack(side=RIGHT, anchor=N, padx=10, pady=10)
+#PORT MENU
+port_var = ttk.StringVar()
+port_menu = ttk.Combobox(root, textvariable=port_var, values=list_serial_ports(), state="readonly")
+port_menu.pack(side=RIGHT, anchor=N, padx=10, pady=0)
 
+
+
+
+
+
+
+
+
+
+
+#START BUTTON
+BT2 = ttk.Button(root, text="Stop", bootstyle=DANGER)
+BT2.pack(pady=10, padx=5, side=RIGHT, anchor=SW)
+
+#STOP BUTTON
 BT1 = ttk.Button(root, text="Start", bootstyle=SUCCESS)
-BT1.pack(pady=20, padx=10, side=RIGHT, anchor=S)
+BT1.pack(pady=10, padx=5, side=RIGHT, anchor=SW)
 
 
-BT2 = ttk.Button(root, text="STOP", bootstyle=DANGER)
-BT2.pack(pady=20, padx=0, side=RIGHT, anchor=S)
+
 
 
 
